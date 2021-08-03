586c0a5fec6f36faf7c8c355abe2a3d33ee8000c
VinnyPushka <snuffle2008@mail.ru>
2021-08-03 22:06:24 +1000

    test.asfaf

---
+++ Olymp.js
@@ -0,0 +1,23 @@
+function triangle(row) {
+    var Final = '';
+    while (row.length > 1){
+        for (var i = 0; i < row.length-1; i++){
+            if (row[i]==row[i+1]){
+            Final+=row[i]
+        } else if (row[i]+row[i+1]=="GB"||row[i]+row[i+1]=="BG"){
+            Final+="R";
+        } else if (row[i]+row[i+1]=="RG"||row[i]+row[i+1]=="GR"){
+            Final+="B";
+        } else if (row[i]+row[i+1]=="BR"||row[i]+row[i+1]=="RB"){
+            Final+="G";
+        };
+        };
+    row = Final;
+    Final = "";
+    };
+    return row;
+    };
+console.log(triangle('GRBGGBRBGGR'))
+
+
+
